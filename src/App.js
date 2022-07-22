b21a4fc22e8d6c0e2c73b90c19284936013b64a2
efontecha <kitservi@hotmail.com>
2022-07-21 19:30:06 -0500

    Creacion proyecto Cli

--- src/App.js
+++ src/App.js
@@ -2,6 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  ///const [count, setcount] = useState(0)
+  // let i=0
+  // i = i+1
+//   let condicion = true
+//  console.log(`El resultado es ${ condicion ? 'verdaddero': 'falso'}`)
+
+//  const array =[1,2,3,4]
+//  const once =11
+
+//  const newarray =[...array,once]
+
+let campoid = '_id'
+const objeto={
+  nombre: 'edgar',
+  ['persona'+campoid]: '5656'
+}
+
+const {nombre: firstName}= objeto
+console.log(firstName)
+
   return (
     <div className="App">
       <header className="App-header">
